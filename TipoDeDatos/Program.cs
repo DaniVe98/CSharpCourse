51866c81d1d30b66ce074e39892d277a079571e9
Daniel Vega <danielalbertovegabejarano@gmail.com>
2024-09-20 17:10:24 -0500

    Tipo de Datos

---
+++ TipoDeDatos/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+int number = 121211;
+double doubleNumber = 12.5;
+char character = 'a';
+string name = "Daniel was here";
+bool isDead = false;
+
+Console.WriteLine(isDead);
