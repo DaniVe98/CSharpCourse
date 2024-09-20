43e7971209a43d0d553b307033e90b7e86985a86
Daniel Vega <danielalbertovegabejarano@gmail.com>
2024-09-20 16:51:37 -0500

    Herencia

---
+++ Herencia/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Doctor doctor1 = new Doctor("Daniel", 25, "Frontend");
+Console.WriteLine(doctor1.GetInfo());
+Console.WriteLine(doctor1.GetData());
+
+Dev dev1 = new Dev("Hector", 10, "JS");
+Console.WriteLine(dev1.GetInfo());
+Console.WriteLine(dev1.GetData());
+
+class People
+{
+    private string _name;
+    private int _age;
+    public People(string name, int age)
+    {
+        _name = name;
+        _age = age;
+    }
+
+    public string GetInfo()
+    {
+        return _name + " " + _age;
+    }
+}
+
+class Doctor : People
+{
+    private string _speciality;
+    public Doctor(string name, int age, string speciality) : base(name, age)
+    {
+        _speciality = speciality;
+    }
+
+    public string GetData()
+    {
+        return GetInfo() + " " + _speciality;
+    }
+}
+
+
+class Dev : People
+{
+    private string _language;
+    public Dev(string name, int age, string language) : base(name, age)
+    {
+        _language = language;
+    }
+
+    public string GetData()
+    {
+        return GetInfo() + " " + _language;
+    }
+}
