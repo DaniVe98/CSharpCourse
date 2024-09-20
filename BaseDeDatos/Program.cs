5ca0a8b405117a6eef1a2ac526a6c07280e33882
Daniel Vega <danielalbertovegabejarano@gmail.com>
2024-09-20 16:28:18 -0500

    Base de Datos

---
+++ BaseDeDatos/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BaseDeDatos;
+using System.Data.SqlClient;
+
+try
+{
+    DB db = new DB(".", "CsharpDB", "sa", "Inchcape@2023<");
+    db.Connect();
+    Console.WriteLine("Si se pudo conectar");
+    db.Close();
+}
+catch (SqlException ex)
+{
+    Console.WriteLine("No pudimos conectarnos. " + ex.Message);
+}
