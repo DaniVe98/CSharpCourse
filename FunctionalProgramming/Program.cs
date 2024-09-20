50a27c9768f9f36f4b0e3df8dd7a1ef23eb4696d
Daniel Vega <danielalbertovegabejarano@gmail.com>
2024-09-20 16:44:00 -0500

    Functional Programming

---
+++ FunctionalProgramming/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Operation mySum = Functions.Sum;
+mySum = Functions.Sum;
+Console.WriteLine(mySum(2,3));
+
+mySum = Functions.Mul;
+Console.WriteLine(mySum(2,3));
+
+Show show = Functions.ConsoleShow;
+show("Hello world");
+
+delegate int Operation(int a, int b);
+delegate void Show(string message);
+public class Functions
+{
+    public static int Sum(int x, int y) => x + y;
+    public static int Mul(int num1, int num2) => num1 * num2;
+    public static void ConsoleShow(string m) => Console.WriteLine(m);
+}
