85fa41d6f4948e91383705885c7e22970a720109
Daniel Vega <danielalbertovegabejarano@gmail.com>
2024-09-20 17:00:37 -0500

    Sobrecarga

---
+++ Sobrecarga/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Math math = new Math();
+Console.WriteLine(math.Sum(1,2));
+Console.WriteLine(math.Sum("3","6"));
+int[] numbers = new int[] {1,2,5};
+Console.WriteLine(math.Sum(numbers));
+
+public class Math
+{
+    public int Sum(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Sum(string a, string b)
+    {
+        return int.Parse(a) + int.Parse(b);
+    }
+
+    public int Sum(int[] numbers)
+    {
+        int result = 0;
+        int i = 0;
+
+        while (i < numbers.Length)
+        {
+            result += numbers[i];
+            i++;
+        }
+
+        return result;
+    }
+}
