8c01cc1b4367d8ed2035b6db9f6d21c697bdd360
Daniel Vega <danielalbertovegabejarano@gmail.com>
2024-09-20 16:59:19 -0500

    Sentencia If - Else If

---
+++ SentenciaIf-ElseIf/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+bool areYouHyngry = true;
+bool youHaveMoney = true;
+
+if (areYouHyngry && youHaveMoney && IsOpenRestaurant("Lonches pepe", 17))
+{
+    Console.WriteLine("Come");
+} else
+{
+    Console.WriteLine("Pues no comas");
+}
+
+static bool IsOpenRestaurant(string name, int hour = 0)
+{
+    if(name == "Lonches pepe" && hour > 8 && hour < 23)
+    {
+        return true;
+    } 
+    else if(name == "Restaurante 24 horas")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
