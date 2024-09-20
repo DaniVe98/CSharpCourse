ab8f1e4684821791d4c4747b850f6dade7533ce5
Daniel Vega <danielalbertovegabejarano@gmail.com>
2024-09-20 16:46:56 -0500

    Functional Programming 4 - Lambda

---
+++ FunctionalProgramming4-Lambda/Program.cs
@@ -0,0 +1,75 @@
+﻿
+//Delegado con un solo parametro (para que funcione, comenta el codigo abajo con 2 parametros):
+
+Operation mySum = Functions.Sum;
+mySum = Functions.Sum;
+Console.WriteLine(mySum(2, 3));
+
+
+
+mySum = Functions.Mul;
+Console.WriteLine(mySum(2, 3));
+
+
+
+Show cw = Console.WriteLine;
+cw += Functions.ConsoleShow;
+//cw("Hello world");
+
+
+
+//Functions.Some("Juan", "Guevara", cw);
+
+
+
+#region Action
+string hi = "Hola";
+Action<string> showMessage = Console.WriteLine;
+Action<string, string> showMessage2 = (a, b) =>
+{
+    Console.WriteLine($"{hi} {a} {b}");
+};
+Action<string, string, string> showMessage3 = (a,b,c) => Console.WriteLine($"Que se dice {a} {b} el {c} crack");
+
+showMessage2("Hector", "De Leon");
+showMessage3("Hector", "De Leon", "Dev");
+Functions.SomeAction("Jaime", "Ramirez", (a) => Console.WriteLine("soy una expresion " + a));
+
+
+//showMessage("hola pepsicola");
+//Functions.SomeAction("Jaime", "Ramirez", showMessage);
+#endregion
+
+
+
+//En vez de ponernos a crear varios delegados, podemos crear delegados con Action que viene por defecto en C#
+#region Delegados
+delegate int Operation(int a, int b);
+public delegate void Show(string message);
+public delegate void Show2(string message, string message2);
+public delegate void Show3(string message, string message2, string message3);
+#endregion
+
+
+
+public class Functions
+{
+    public static int Sum(int x, int y) => x + y;
+    public static int Mul(int num1, int num2) => num1 * num2;
+    public static void ConsoleShow(string m) => Console.WriteLine(m.ToUpper());
+    public static void Some(string name, string lastName, Show fn)
+    {
+        Console.WriteLine("Hago algo al inicio");
+        fn($"Hola {name} {lastName}");
+        Console.WriteLine("Hago algo al final");
+    }
+
+
+
+    public static void SomeAction(string name, string lastName, Action<string> fn)
+    {
+        Console.WriteLine("El secreto es");
+        fn($"Mi amor es {name} {lastName}");
+        Console.WriteLine("Finalmente confesado");
+    }
+}
