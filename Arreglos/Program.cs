b4b5c9986e0b78c7017462b8391ef8bbb9a0b34f
Daniel Vega <danielalbertovegabejarano@gmail.com>
2024-09-20 16:27:10 -0500

    Arreglos

---
+++ Arreglos/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] friends = new string[8]
+{
+    "Pancho",
+    "Paco",
+    "Ana",
+    "Ruben",
+    "Carla",
+    "Luis",
+    null,
+    null
+};
+
+friends[0] = "Hector";
+
+Console.WriteLine(friends[0]);
+Console.WriteLine(friends[1]);
+Console.WriteLine(friends[2]);
+Console.WriteLine(friends[3]);
+Console.WriteLine(friends[4]);
+Console.WriteLine(friends[5]);
+Console.WriteLine(friends[6]);
+
+friends[7] = "Carlos";
+Console.WriteLine(friends[7]);
