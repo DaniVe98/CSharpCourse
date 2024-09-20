aba8a10c3d3fbbbb5d2bac389b6956440f9f6edf
Daniel Vega <danielalbertovegabejarano@gmail.com>
2024-09-20 16:29:58 -0500

    Base de Datos 2

---
+++ BaseDeDatos2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BaseDeDatos2;
+using System.Data.SqlClient;
+
+try
+{
+    BeerDB beerDB = new BeerDB(".", "CsharpDB", "sa", "Inchcape@2023<");
+    List<Beer> beers = beerDB.GetAll();
+
+    foreach(var beer in beers)
+    {
+        Console.WriteLine(beer.Name);
+    }
+}
+catch (SqlException ex)
+{
+    Console.WriteLine("No pudimos conectarnos. " + ex.Message);
+}
